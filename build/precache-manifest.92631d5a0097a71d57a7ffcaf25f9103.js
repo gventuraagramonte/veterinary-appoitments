c3687b0d6f4ef137f1075bab587d64e79c4de822
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-05-10 01:09:23 -0500

    updating build

---
+++ build/precache-manifest.92631d5a0097a71d57a7ffcaf25f9103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfc762543d0e85b01d7c14d52cc951e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b66d930c8af48a7404",
+    "url": "/static/css/main.c422e52a.chunk.css"
+  },
+  {
+    "revision": "8befaa90c1ffff0f7433",
+    "url": "/static/js/2.64618b74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64618b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b66d930c8af48a7404",
+    "url": "/static/js/main.31555a1a.chunk.js"
+  },
+  {
+    "revision": "555038815408fd385bd2",
+    "url": "/static/js/runtime-main.8d8bfb7d.js"
+  }
+]);
